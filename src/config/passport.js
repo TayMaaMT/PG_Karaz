6c15627d2885aa723ceaaee4934810ee9e4c1306
taymaa <taymaa.2917@gmail.com>
2020-03-19 19:44:40 +0200

    add redis

--- src/config/passport.js
+++ src/config/passport.js
@@ -17,15 +17,15 @@ passport.use(
     new facebookeStrategy(FacebookOption, async(accessToken, refreshToken, profile, done) => {
         try {
             const currentUser = await findOne('users', { fb_id: profile._json.id });
-            if (currentUser) {
+            if (currentUser[0]) {
                 // if the user is not new (has created )
-                done(null, currentUser);
+                done(null, currentUser[0]);
             } else {
                 // if the user is new (not created yet)
                 const user = await findOne('users', { email: profile._json.email });
-                if (user) {
-                    await update('users', user, { fb_id: profile._json.id });
-                    done(null, user);
+                if (user[0]) {
+                    await update('users', user[0], { fb_id: profile._json.id });
+                    done(null, user[0]);
                 } else {
                     const user_id = await creat('users', {
                         name: profile._json.name,
@@ -34,7 +34,7 @@ passport.use(
                         is_verified: true
                     });
                     const newUser = await findOne('users', { fb_id: profile._json.id });
-                    done(null, newUser);
+                    done(null, newUser[0]);
                 }
             }
         } catch (err) {
@@ -51,17 +51,17 @@ passport.use(
         try {
 
             const currentUser = await findOne('users', { google_id: profile.id });
-            if (currentUser) {
+            if (currentUser[0]) {
                 // if the user is not new (has created )
-                done(null, currentUser);
+                done(null, currentUser[0]);
             } else {
                 // if the user is new (not created yet)
                 const user = await findOne('users', { email: profile.email });
 
-                if (user) {
+                if (user[0]) {
 
-                    await update('users', user, { google_id: profile.id, is_verified: profile.email_verified });
-                    done(null, user);
+                    await update('users', user[0], { google_id: profile.id, is_verified: profile.email_verified });
+                    done(null, user[0]);
                 } else {
                     const reg_date = new Date();
                     const user_id = await creat('users', {
@@ -73,7 +73,7 @@ passport.use(
                     });
 
                     const newUser = await findOne('users', { google_id: profile.id });
-                    done(null, newUser);
+                    done(null, newUser[0]);
                 }
             }
         } catch (err) {
